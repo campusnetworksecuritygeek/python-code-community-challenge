ef874bebd3d910f2af0e21ebf96b8ed9490ae958
campusnetworksecuritygeek <65725765+campusnetworksecuritygeek@users.noreply.github.com>
2020-05-23 18:40:47 +0200

    Create data_netmiko.py

---
+++ Backup_MR_Python/Netmiko/data_netmiko.py
@@ -0,0 +1,23 @@
+# This file has devices information, it's look like a database.
+# By morad rachad
+
+# All devices Information in those dictionaries
+RDC = {'device_type': 'cisco_ios', 'ip': '192.168.215.101', 'username': 'cisco', 'password': '123'}
+Etage_1 = {'device_type': 'cisco_ios', 'ip': '192.168.215.102', 'username': 'cisco', 'password': '123'}
+Etage_2 = {'device_type': 'cisco_ios', 'ip': '192.168.215.103', 'username': 'cisco', 'password': '123'}
+Etage_3 = {'device_type': 'cisco_ios', 'ip': '192.168.215.104', 'username': 'cisco', 'password': '123'}
+Etage_4 = {'device_type': 'cisco_ios', 'ip': '192.168.215.105', 'username': 'cisco', 'password': '123'}
+
+# All devices name's in those variables
+RDC_name = 'RDC'
+Etage_1_name = 'Etage_1'
+Etage_2_name = 'Etage_2'
+Etage_3_name = 'Etage_3'
+Etage_4_name = 'Etage_4'
+
+# stock both the devices information and names in those dictionaries
+Sw0 = (RDC, RDC_name)
+Sw1 = (Etage_1, Etage_1_name)
+Sw2 = (Etage_2, Etage_2_name)
+Sw3 = (Etage_3, Etage_3_name)
+Sw4 = (Etage_4, Etage_4_name)
